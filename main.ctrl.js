94e5513353eb0289b686caf4d7d9c5500f447d8a
Robin Bonatesta <robinbonatesta@gmail.com>
2015-02-16 22:55:53 -0500

    added finished files

--- main.ctrl.js
+++ main.ctrl.js
@@ -1,5 +1,41 @@
 angular.module('app').controller("MainController", ['$http', function($http){
     var vm = this;
     vm.title = 'AngularJS Tutorial Example';
-  
+    vm.searchInput = '';
+    vm.members = [
+        {
+            name: 'Robin',
+            color: 'chartreuse',
+        },
+        {
+            name: 'Paul',
+            color: 'blue??',
+        },
+        {
+            name: 'Henry',
+            color: 'marine blue',
+        },
+        {
+            name: 'Brad',
+            color: 'army green',
+        },
+    ];
+    vm.orders = [
+        {
+            id: 0,
+            title: 'Name',
+            key: 'name',
+        },
+        {
+            id: 1,
+            title: 'Color',
+            key: 'color',
+        }
+    ];
+    vm.order = vm.orders[0];
+    vm.new = {};
+    vm.addMember = function() {
+        vm.members.push(vm.new);
+        vm.new = {};
+    };
 }]);
