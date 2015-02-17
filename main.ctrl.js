179e9794ec5655577d7236c65156df0509062929
Paul Dilyard <paul.dilyard@gmail.com>
2015-02-16 23:19:30 -0500

    Modified to use API

--- main.ctrl.js
+++ main.ctrl.js
@@ -1,41 +1,50 @@
-angular.module('app').controller("MainController", ['$http', function($http){
+angular.module('app').controller("MainController", ['$http', function($http) {
     var vm = this;
     vm.title = 'AngularJS Tutorial Example';
     vm.searchInput = '';
-    vm.members = [
-        {
-            name: 'Robin',
-            color: 'chartreuse',
-        },
-        {
-            name: 'Paul',
-            color: 'blue??',
-        },
-        {
-            name: 'Henry',
-            color: 'marine blue',
-        },
-        {
-            name: 'Brad',
-            color: 'army green',
-        },
-    ];
-    vm.orders = [
-        {
-            id: 0,
-            title: 'Name',
-            key: 'name',
-        },
-        {
-            id: 1,
-            title: 'Color',
-            key: 'color',
-        }
-    ];
-    vm.order = vm.orders[0];
+
+    function get() {
+        $http
+            .get('http://104.236.35.212/links')
+            .success(function (data) {
+                vm.links = data.links;
+            })
+            .error(function (data, status) {
+                console.log(status);
+                console.log(data);
+            });
+    }
+    get();
+
     vm.new = {};
-    vm.addMember = function() {
-        vm.members.push(vm.new);
+    vm.addLink = function() {
+        var config = {
+            method: 'POST',
+            url: 'http://104.236.35.212/links/create',
+            headers: {
+                'Authorization': 'Basic dXNlcjpwYXNz',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            transformRequest: function(obj) {
+                var str = [];
+                for(var p in obj)
+                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            },
+            data: {
+                url: vm.new.url,
+                title: vm.new.title
+            }
+        };
+        $http(config)
+            .success(function (data) {
+                get();
+            })
+            .error(function (data, status) {
+                alert('Failed to add link');
+                console.log(data);
+                console.log(status);
+            });
         vm.new = {};
     };
 }]);
